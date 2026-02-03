6bd8311062a58991e24a7f9f04a618db71c2d5f3
nithiroj <nithiroj.n@kkumail.com>
2026-02-03 17:24:39 +0700

    fix lab5pox configuration

--- lab5/lab5pox.sh
+++ lab5/lab5pox.sh
@@ -1,10 +1,12 @@
 #open in another tab
+cd pox
+sudo ./pox.py --verbose forwarding.l2_learning
+
+#open in another tab
 sudo mn --mac --switch=ovsk --controller=remote,ip=127.0.0.1,port=6633
 dpctl dump-flows
 h1 arp
 h1 ping -c h2
+
 #open in another tab
 sudo wireshark
-#open in another tab
-cd pox
-sudo ./pox.py --verbose forwarding.l2_learning
